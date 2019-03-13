c78ee8ef1c896a0fe15336aad3f086e110bde681
Edward Goodhew <ejgoodhew@btinternet.com>
2019-03-13 16:09:55 +0000

    Stuff done

---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigation : MonoBehaviour
+{
+
+    public void ChangeToGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+public void ChangeToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
