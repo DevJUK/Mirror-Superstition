33a9b780b03d1572a124b3079a58340f82134a38
Edward Goodhew <ejgoodhew@btinternet.com>
2019-03-13 13:50:45 +0000

    Scripts added

---
+++ Assets/Scripts/MirrorBreakScrpt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorBreakScrpt : MonoBehaviour
+{
+    public float Range;
+    public MirrorScrpt MirrorScrpt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit Hit;
+        if (Physics.Raycast(transform.position,transform.forward, out Hit, Range))
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                // play punch animation
+                Hit.transform.GetComponent<MirrorScrpt>().Broken = true;
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+               // play punch animation
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreTimerScrpt.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTimerScrpt : MonoBehaviour
+{
+    public int Score;
+    public int Multiplyer;
+
+    public float Timer;
+    public float TimerTarget;
+    public bool TimerRunning;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Timer < TimerTarget && TimerRunning)
+        {
+            Timer += Time.deltaTime;
+        }
+        else if (Timer >= TimerTarget && TimerRunning)
+        {
+            TimerRunning = false;
+        }
+    }
+
+    public void RestartTimer()
+    {
+        Timer = 0;
+        TimerRunning = true;
+    }
+}
